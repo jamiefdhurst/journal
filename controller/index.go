1b45f723bae02e5d0d7eeac0b9181df62144eb86
Jamie Hurst <jamie@jamiehurst.co.uk>
2016-03-20 10:26:09 +0000

    Built in validation and saving, think version 0.1 is done

--- controller/index.go
+++ controller/index.go
@@ -14,6 +14,7 @@ type Index struct {
 
 type indexData struct {
 	Journals []model.Journal
+	Saved    bool
 }
 
 // Run Index
@@ -22,7 +23,11 @@ func (c *Index) Run(w http.ResponseWriter, r *http.Request) {
 	js := model.Journals{}
 	js.SetDb(c.Db)
 	js.FetchAll()
-	data := indexData{js.Journals}
+	data := indexData{js.Journals, false}
+	query := r.URL.Query()
+	if query["saved"] != nil {
+		data.Saved = true
+	}
 
 	t, _ := template.ParseFiles("./src/journal/views/_layout/header.tmpl", "./src/journal/views/_layout/footer.tmpl", "./src/journal/views/index.tmpl")
 	t.ExecuteTemplate(w, "header", nil)
--- controller/new.go
+++ controller/new.go
@@ -15,17 +15,28 @@ type New struct {
 // Run NewC
 func (c *New) Run(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "GET" {
+		data := map[string]bool{
+			"Error": false,
+		}
+		query := r.URL.Query()
+		if query["error"] != nil {
+			data["Error"] = true
+		}
+
 		t, _ := template.ParseFiles("./src/journal/views/_layout/header.tmpl", "./src/journal/views/_layout/footer.tmpl", "./src/journal/views/new.tmpl")
 		t.ExecuteTemplate(w, "header", nil)
-		t.ExecuteTemplate(w, "content", nil)
+		t.ExecuteTemplate(w, "content", data)
 		t.ExecuteTemplate(w, "footer", nil)
 		t.Execute(w, nil)
 	} else {
+		if r.FormValue("title") == "" || r.FormValue("date") == "" || r.FormValue("content") == "" {
+			http.Redirect(w, r, "/new?error=1", 302)
+		}
 
 		js := model.Journals{}
 		js.SetDb(c.Db)
 		js.Create(0, model.Slugify(r.FormValue("title")), r.FormValue("title"), r.FormValue("date"), r.FormValue("content"))
 
-		http.Redirect(w, r, "/", 302)
+		http.Redirect(w, r, "/?saved=1", 302)
 	}
 }
